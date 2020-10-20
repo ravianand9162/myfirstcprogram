9697b3942e688314eb4ffd96b072ae4ca82602fb
ravi <ravianand9162745761@gmail.com>
2020-10-20 21:56:55 +0530

    myfirstrepository

---
+++ first_class.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("Hello  World This is C PRograming language");
+    return 0;
+}
+
+// Syntax
+// String = Any thing inside double inverted comma is known as Strings
---
+++ second_class.c
@@ -0,0 +1,45 @@
+// Comments
+// Variable 
+// a = 3; // a me 3 ko assign kiya gya hai
+// ravi = 's'; // variable ravi naam ka jisme s assign kiya hua hai
+
+// Rules For assigning Variables :
+// 1) first character shoud be an alphabet ya fir _ underscore
+// ex : ravi, mohan , _sujay 
+// comma ya space se nai assign kr skte
+// ex : ,ravi | ra vi |
+// koi v special symbols v use nai krskte variable assign krne ke liye @ #$%^&*
+// variable is case sensitive
+// ravi --> kyuki ye wala lower case mei hai
+// Ravi  --> ye wala upper case mei hai
+// agar case sensitive hota hai to ye dono alag alag variable hai
+
+// humlog ko variable ka naam hmesa kuch meaning ful rakhna chahiye as per convention
+// positive_numbers = 4;
+// negative_numbers = -5;
+
+// a = 4;
+// b = -5;
+
+// Types of constant
+
+// Real Constant ----> 8.5, 5.0
+// Character Constant --> 'a', 'b', 'C', 'D'  ye single inverted comma ke andar jo v hai characters wo character constant hai
+// Integer Constant ---> 1,2,3,4,5,6
+
+
+// Keyword
+// some reserved keyword that  is already in use by particular programming language
+// Total there are 32 keywords in c programming language
+// example :
+// return ,int if elsedo while viod
+// postive_number = 4 ;
+// if = 4; // this will give u errror because this has a keyword which is reserved by c
+// ravi = 5;
+
+// Basic Structure of C Programming Language
+// main() --- koi v program run krna shuru krega
+// har statemnet is terminated by semi-colon (;)
+// koi v statement jo hota hai wo case - sensitive hota hai
+
+
